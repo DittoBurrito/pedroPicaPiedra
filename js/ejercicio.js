918b4ab1d7c34110266aeec76fdb0a7e283f9f4b
DittoBurrito <davidaler45@gmail.com>
2024-10-30 17:44:19 -0500

    arreglo de errores y creacion de respuesta de caso 3 (valor de producto mayor a 50.000)

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,14 +1,17 @@
-let respuestaEncuesta = prompt("¿Desea ingresar un producto? 1. Sí 2. No");
+let respuestaPreguntaProducto = prompt("¿Desea ingresar un producto? 1. Sí 2. No");
 
-while (respuestaEncuesta == "1") {
+while (respuestaPreguntaProducto == "1") {
     let valorProducto = parseFloat(prompt("Ingrese valor del producto"));
     let ganancia = valorProducto * 0.05;
 
     if (valorProducto < 10000) {
         alert("La ganancia es " + ganancia);
-    } else if (valorProducto >= 10000 && valorProducto < 50000){
+    } else if (valorProducto >= 10000 && valorProducto < 50000) {
         alert("El valor del producto supera los 10.000, la ganancia es " + (ganancia * 2));
-    
-    
-        respuestaEncuesta = prompt("¿Desea ingresar otro producto? 1. Sí 2. No");
+    } else if (valorProducto >= 50000) {
+        alert("El valor del producto supera los 50.000, la ganancia es " + (ganancia * 3));
+    }
+
+    // Pregunta si desea ingresar otro producto
+    respuestaPreguntaProducto = prompt("¿Desea ingresar otro producto? 1. Sí 2. No");
 }

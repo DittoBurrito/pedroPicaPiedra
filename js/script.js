842b59e2a2e3b86d98cfc3878c6019fa41b9af5c
DittoBurrito <davidaler45@gmail.com>
2024-10-30 16:38:35 -0500

    avance uno

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ let usuarios = [{
     "usuario":"admin",
     "contrasena": 1234
 }]
+let intentos = 3
 
-let usuario = document.querySelector("#usuario").value 
-alert(usuario)
+let user = document.getElementById("usuario").value
+let password = document.getElementById("contrasena").value
+
+function ValidarUsuario (usuario, contrasena){
+    for (let index = 1; index <= intentos; index++) {
+        if (usuarios.usuario == usuario && usuarios.contrasena ==contrasena){
+            window.location.href = "/pages/home.html"
+        }
+        
+    }
+}
+
